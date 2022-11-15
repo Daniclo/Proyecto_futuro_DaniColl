46bf793795d07b1546c56b7f07228fdf564e190d
Daniel <danielcollfdz@gmail.com>
2022-11-15 18:22:21 +0100

    Commit master 1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[]args){
+        Veredicto v = new Veredicto();
+
+        System.out.println("Estudiante: " + args[0]);
+        v.v(args[1], args[2]);
+
+    }
+}
---
+++ src/Veredicto.java
@@ -0,0 +1,12 @@
+public class Veredicto {
+
+    public void v(String valor1, String valor2){
+        if (valor1.equals(valor2)){
+            System.out.println("Bien pero puedes estudiar más.");
+        } else if (Integer.valueOf(valor1)>Integer.valueOf(valor2)) {
+            System.out.println("Hace falta más estudio.");
+        }else {
+            System.out.println("Estudias mucho, bien hecho.");
+        }
+    }
+}
