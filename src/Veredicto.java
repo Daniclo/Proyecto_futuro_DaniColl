c3ec669ffbca8372a5e0244db47759d685d69903
Daniel <danielcollfdz@gmail.com>
2022-11-15 18:28:19 +0100

    Commit final

--- src/Veredicto.java
+++ src/Veredicto.java
@@ -1,6 +1,6 @@
 public class Veredicto {
 
-    private void v(String valor1, String valor2){
+    public void v(String valor1, String valor2){
         if (valor1.equals(valor2)){
             System.out.println("Bien pero puedes estudiar más.");
         } else if (Integer.valueOf(valor1)>Integer.valueOf(valor2)) {
